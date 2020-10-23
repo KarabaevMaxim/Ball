82e91ce9ce831990b01f63b12d757ef736d0f605
Karabaev_MO <karabaev_mo@directum.ru>
2020-10-23 08:19:25 +0400

    Добавил возможность запуска игры с игровой сцены без исключений

--- Assets/Scripts/Application.Management/CurrentUserInfoService.cs
+++ Assets/Scripts/Application.Management/CurrentUserInfoService.cs
@@ -14,24 +14,41 @@ namespace Application.Management
 
     #endregion
 
+    #region Поля
+
+    private User _currentUser;
+    
+    #endregion
+    
     #region ICurrentUserInfoService
 
-    public User CurrentUser { get; private set; }
+    public User CurrentUser
+    {
+      // если null, значит пропустили шаг регистрации в главном меню
+      get =>
+        _currentUser ?? (_currentUser = new User
+        {
+          Id = Guid.NewGuid(),
+          Name = "Default user",
+          BestResult = 0
+        });
+      set => _currentUser = value;
+    }
 
     public async Task RegisterAsync(string name)
     {
-      CurrentUser = (await _usersStorage.ReadAllAsync()).FirstOrDefault(u => u.Name.Equals(name));
+      _currentUser = (await _usersStorage.ReadAllAsync()).FirstOrDefault(u => u.Name.Equals(name));
       
-      if (CurrentUser == null)
+      if (_currentUser == null)
       {
-        CurrentUser = new User
+        _currentUser = new User
         {
           Id = Guid.NewGuid(),
           Name = name,
           BestResult = 0
         };
         
-        await _usersStorage.SaveAsync(CurrentUser);
+        await _usersStorage.SaveAsync(_currentUser);
       }
     }
 
